6c945f94e3b0ad93bb9042f967acd27a6cb0ca0a
Mazenhassan4 <121609325+Mazenhassan4@users.noreply.github.com>
2022-12-30 14:30:59 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,32 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace perfect_numbers
+namespace prime_numbers
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int tmp ;
+            int tmp = 0;
             Console.WriteLine("Enter the number to start counting");
-            int num1 =int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
             Console.WriteLine("Enter the number to stop counting");
-            int num2 =int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
             for (int i = num1; i <= num2; i++)
             {
-                tmp =0;
-                for (int c = 1; c <= i/2; c++)
+                for (int c = 1; c <= i; c++)
                 {
-                    if (i%c==0)
+                    if (i % c == 0)
                     {
-                        tmp += c;
-                    }   
+                        tmp++;
+                    }
                 }
-                if (tmp==i)
+
+                if (tmp == 2)
                 {
-                    Console.WriteLine("the perfect number is:{0}",i);
+
+                    Console.WriteLine("the prime number is:{0}",i) ;
+                    tmp = 0;    
+                }
+                else
+                {
+                    tmp = 0;
                 }
             }
         }
     }
 }
+
+
+
+
+            
+        
+
+    
+
