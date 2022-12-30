ad58d710894a367b12f4e7a5b5629b27c501f76e
Mazenhassan4 <121609325+Mazenhassan4@users.noreply.github.com>
2022-12-30 14:24:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfect_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tmp ;
+            Console.WriteLine("Enter the number to start counting");
+            int num1 =int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number to stop counting");
+            int num2 =int.Parse(Console.ReadLine());
+            for (int i = num1; i <= num2; i++)
+            {
+                tmp =0;
+                for (int c = 1; c <= i/2; c++)
+                {
+                    if (i%c==0)
+                    {
+                        tmp += c;
+                    }   
+                }
+                if (tmp==i)
+                {
+                    Console.WriteLine("the perfect number is:{0}",i);
+                }
+            }
+        }
+    }
+}
